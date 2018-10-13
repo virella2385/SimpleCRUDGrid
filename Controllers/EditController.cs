966204e8dda95a0d361b79a553f4316685436eb4
alakratt <alakratt@gmail.com>
2018-10-13 13:08:56 -0400

    Added Edit view and controller

---
+++ Controllers/EditController.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SimpleCRUDGridWebApp.Models;
+using SimpleCRUDGridWebApp.Models.ViewModels;
+using SimpleCRUDGridWebApp.Services;
+
+namespace SimpleCRUDGridWebApp.Controllers
+{
+    public class EditController : Controller
+    {
+        //public List<SelectListItem> customers { get; set; }
+        private ISqlGridData _data;
+
+        public EditController(ISqlGridData data)
+        {
+            _data = data;
+        }
+
+        public IActionResult Index(int id)
+        {
+            EditExpenseVM editExpense = new EditExpenseVM();
+            //editExpense = _data.GetExpense(id);
+
+            Expense currentExpense = new Expense();
+            currentExpense = _data.GetExpense(id);
+            editExpense.customerId = currentExpense.Project.Customer.CustomerId;
+            editExpense.projectId = currentExpense.Project.ProjectId;
+            editExpense.expenseId = currentExpense.ExpenseId;
+            editExpense.expenseName = currentExpense.ExpenseName;
+            editExpense.expenseDate = currentExpense.ExpenseDate;
+            editExpense.expenseDescription = currentExpense.Description;
+            editExpense.expenseAmount = currentExpense.Amount;
+            editExpense.customers = new List<SelectListItem>();
+            editExpense.projects = new List<SelectListItem>();
+
+            //List<SelectListItem> customers = new List<SelectListItem>();
+            editExpense.customers.Add(new SelectListItem { Text = "Select a customer" });
+            foreach (Customer cust in _data.GetCustomers())
+            {
+                editExpense.customers.Add(new SelectListItem
+                {
+                    Text = cust.CustomerName,
+                    Value = cust.CustomerId.ToString()
+                });
+            }
+
+            //List<SelectListItem> projects = new List<SelectListItem>();
+            editExpense.projects.Add(new SelectListItem { Text = "Select a project" });
+            foreach (Project proj in _data.GetProjects(editExpense.customerId))
+            {
+                editExpense.projects.Add(new SelectListItem
+                {
+                    Text = proj.ProjectName,
+                    Value = proj.ProjectId.ToString()
+                });
+            }
+            return View(editExpense);
+        }
+
+        [HttpPostAttribute]
+        public IActionResult SaveForm(EditExpenseVM editExpense, string btnSave) 
+        {
+        	//return new JsonResult(editExpense);
+        	if (ModelState.IsValid) {
+        		Expense modifiedExpense = new Expense();
+        		modifiedExpense = _data.GetExpense(editExpense.expenseId);
+        		modifiedExpense.Amount = editExpense.expenseAmount;
+        		modifiedExpense.ExpenseName = editExpense.expenseName;
+        		modifiedExpense.ExpenseId = editExpense.expenseId;
+        		modifiedExpense.Description = editExpense.expenseDescription;
+        		modifiedExpense.Project.ProjectId = editExpense.projectId;
+        		modifiedExpense.Project.Customer.CustomerId = editExpense.customerId;
+				_data.UpdateExpense(modifiedExpense);
+
+				if (btnSave.ToLower() == "save")
+				{
+					return RedirectToAction("Index", "Home");
+				}
+	        	/*switch (btnSave.ToLower())
+	        	{
+	        		case "save": return RedirectToAction("Index", "Home");
+	        			//return new JsonResult("save");
+	        		default: return return
+
+	        			//return new JsonResult("apply");
+	        	}*/
+	        }
+	        return View();
+        }
+
+        [HttpGetAttribute]
+        public JsonResult GetProjects(int id)
+        {
+            return new JsonResult(_data.GetProjects(id));
+        }
+
+         [HttpGetAttribute]
+        public JsonResult GetExpense()
+        {
+            return new JsonResult(_data.GetExpense(1));
+        }
+
+
+    }
+}
---
+++ Models/ViewModels/EditExpenseVM.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCRUDGridWebApp.Models.ViewModels
+{
+    public class EditExpenseVM
+    {
+        [RequiredAttribute(ErrorMessage = "Selecting a customer is required")]
+        [DisplayAttribute(Name = "Select a customer")]
+        public int customerId { get; set; }
+
+        [RequiredAttribute(ErrorMessage = "Selecting a project is required")]
+        [DisplayAttribute(Name = "Select a project")]
+        public int projectId { get; set; }
+
+        public int expenseId { get; set; }
+
+        [RequiredAttribute]
+        [DisplayAttribute(Name = "Name")]
+        public string expenseName { get; set; }
+
+        [RequiredAttribute]
+        [DisplayAttribute(Name = "Amount")]
+        public decimal expenseAmount { get; set; }
+
+        [DisplayAttribute(Name = "Description")]
+        public string expenseDescription { get; set; }
+
+        [RequiredAttribute]
+        [DataType(DataType.Date)]
+        [DisplayAttribute(Name = "Date")]
+        public DateTime expenseDate { get; set; }
+        public List<SelectListItem> customers { get; set; }
+        public List<SelectListItem> projects { get; set; }
+
+    }
+}
